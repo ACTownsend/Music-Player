7ed00c8b844b7ad41a0579ced213e5e823d74f96
Adam Townsend <townse42@coventry.ac.uk>
2023-03-08 18:56:21 +0000

    Beginning to implement backend support with mongodb

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const app = express();
 
 
 const cors = require("cors");
+const {default:mongoose} = require("mongoose");
+
+
 
 app.get("/", (req, res) =>{
     return res.json("Testing Testing 123")
