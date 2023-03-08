37cd9bf8088505f9b89f58859d08fd6cc0278bcb
Adam Townsend <townse42@coventry.ac.uk>
2023-03-08 17:29:41 +0000

    Attempting to integrate styles to html

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import {FcGoogle} from "react-icons/fc"
+
+const Login = () => {
+  return (
+    <div classname="log">
+      <div classname="log2">
+        <div classname = "log3">
+          <div classname="log4"> 
+            <FcGoogle />
+            Sign in with Google
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+  
+export default Login
