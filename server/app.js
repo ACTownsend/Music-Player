08a412a3f38370ec0064fa9ecfd6a279cf177082
Adam Townsend <townse42@coventry.ac.uk>
2023-03-14 20:34:33 +0000

    Set up backend for adding/removing songs

--- server/app.js
+++ server/app.js
@@ -5,13 +5,22 @@ require("dotenv/config")
 const cors = require("cors");
 const {default:mongoose} = require("mongoose");
 
-//app.use(cors({origin: true}));
+app.use(cors({origin: true}));
+app.use(express.json())
 
 app.get("/", (req, res) =>{
     return res.json("Testing Testing 123")
 })
 
-//user authentication route
+//artist routes
+const artistsRoutes = require("./routes/artist");
+app.use("api/artist/", artistsRoutes);
+//album routes
+const albumRoutes = require("./routes/albums");
+app.use("api/albums/", albumRoutes);
+//song routes
+const songRoutes = require("./routes/song");
+app.use("api/song/", songRoutes);
 
 mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true});
 mongoose.connection
